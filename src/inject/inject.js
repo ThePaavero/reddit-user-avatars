4c9ebf441d24c62db32b4fa6fdc6d1a926854d85
Pekka <nospam@astudios.org>
2017-10-14 19:14:11 +0300

    Started on foreground color switching.

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,13 +1,13 @@
 const RedditUserAvatars = () => {
 
-  const usernamesToColors = {}
+  const usernamesToBackgroundColors = {}
   const elementPool = []
 
   const populateUserPool = () => {
     const usernameElements = document.querySelectorAll('.commentarea a.author')
     Array.from(usernameElements).forEach(el => {
       const username = el.innerText.trim()
-      usernamesToColors[username] = getRandomColorHex(username)
+      usernamesToBackgroundColors[username] = getRandomColorHex(username)
       elementPool.push(el)
     })
   }
@@ -27,18 +27,35 @@ const RedditUserAvatars = () => {
   }
 
   const render = () => {
-    for (let i in usernamesToColors) {
+    for (let i in usernamesToBackgroundColors) {
       const username = i
-      const color = usernamesToColors[i]
-      const avatar = getAvatarElement(username, color)
+      const backgroundColor = usernamesToBackgroundColors[i]
+      const avatar = getAvatarElement(username, backgroundColor)
       elementPool.forEach(el => {
         if (el.innerText.trim() === username) {
           const clonedAvatar = avatar.cloneNode(true)
           el.style.position = 'relative'
+          el.style.backgroundColor = getSuitableForegroundColorForBackground(backgroundColor)
           el.parentNode.insertBefore(clonedAvatar, el)
         }
       })
     }
+  }
+
+  const getSuitableForegroundColorForBackground = (backgroundColor) => {
+    const rgb = hexToRgb(backgroundColor)
+    const brightness = ((rgb.r * 299) + (rgb.g * 587) + (rgb.b * 114)) / 1000
+    return brightness < 150 ? '#fff' : '#000'
+  }
+
+  const hexToRgb = (hex) => {
+    // https://stackoverflow.com/a/5624139
+    const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex)
+    return result ? {
+      r: parseInt(result[1], 16),
+      g: parseInt(result[2], 16),
+      b: parseInt(result[3], 16)
+    } : null
   }
 
   const getAvatarElement = (username, color) => {
